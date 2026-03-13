aa3f59abe0482fde3e3756db7d7213910bfdf14f
Junak12 <pauljunak@gmail.com>
2026-03-14 03:49:15 +0600

    make a connection with mongodb and create a userCollection

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+dotenv.config();
+const app = express();
+
+const port = process.env.Port;
+
+//pauljunak_db_user
+//d40h7Rvcjnqh6MkJ
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('GoTicket is running');
+})
+
+const uri = process.env.MONGODB_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("goTicket");
+    const userCollection = db.collection('user');
+
+    app.post('/user', async(req, res) => {
+        const user = req.body;
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
